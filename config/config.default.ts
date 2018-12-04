da1a7463404893e1f804c21b629cec332e4822e7
yidafu <tiaozuwushi@qq.com>
2018-12-04 11:19:36 +0800

    rm semicolom

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,23 +1,29 @@
-import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg'
 
 export default (appInfo: EggAppInfo) => {
-  const config = {} as PowerPartial<EggAppConfig>;
+  const config = {} as PowerPartial<EggAppConfig>
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1543766398816_4130';
+  config.keys = appInfo.name + '_1543766398816_4130'
 
   // add your egg config in here
-  config.middleware = [];
+  config.middleware = []
 
   // add your special config in here
   const bizConfig = {
     sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
-  };
+  }
 
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'egg-sequelize-doc-default',
+  }
   // the return config will combines to EggAppConfig
   return {
     ...config,
     ...bizConfig,
-  };
-};
+  }
+}
