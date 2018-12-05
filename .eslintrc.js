59b6a1d44e2ab5083524723305ef1629c2b09d0a
yidafu <tiaozuwushi@qq.com>
2018-12-06 00:43:00 +0800

    use eslint to app/model and database's js files

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "no-debugger": "warn",
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-console": "warn",
+    "no-unused-vars": 1
+  }
+}
